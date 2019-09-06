bda73b180862b505f8bbc4278bd714a0b6811325
Abhishekparmar123 <53890201+Abhishekparmar123@users.noreply.github.com>
2019-09-06 21:43:52 +0530

    Add files via upload

---
+++ abhi.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration Form</title>
+	<link rel="stylesheet" type="text/css" href="abhi.css">
+</head>
+<body>
+
+<div id="one">
+	<form action="abhi1.php" method="post">
+<p>
+	<label>Username</label>
+<input type="text" name="user" id="three" name="user" />
+</p>
+<p>
+	<label>Password</label>
+<input type="password" name="pass" id="three" name="pass" />
+</p>
+<p>
+	
+<input type="submit" name="btn"  id="two" value="Register" />
+</p>
+
+</form>
+
+	</div>
+
+</body>
+</html>
